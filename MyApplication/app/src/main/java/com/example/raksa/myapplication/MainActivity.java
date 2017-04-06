be4254ab1161b1d44ed369814698eab9d3232a36
BamBo <raksameth@gmail.com>
2017-04-06 18:47:31 +0700

    Fragment Attached And Layout Modified

--- MyApplication/app/src/main/java/com/example/raksa/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/raksa/myapplication/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.raksa.myapplication;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentManager fragmentManager = getFragmentManager();
+    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //attach the Fragment to the container..
+        CalculateFragment fragment = new CalculateFragment();
+        //Check if there is existed fragment in the container..
+        Fragment ifExisted = fragmentManager.findFragmentById(R.id.container);
+        if (ifExisted==null){
+
+        }
+
     }
 }
